d47ca5d58ba3d721f8110d465238006217207787
ArturRochel <arturrochel@gmail.com>
2023-11-22 09:14:56 -0300

    Terceira Atualização

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 let saldo = level(10, 5)
 
-console.log(`O saldo de vítorias desse jogador é ${saldo}`)
+console.log(`O Heroi tem o saldo de ${saldo[0]} vitórias e está no nível de ${saldo[1]}.`)
 
-function level(victories, defeats){
+function level(victories, defeats, name, nickName,splitChar){
     let balance = victories - defeats
-    return balance
+    let ranked = ""
+    if(balance <= 10){
+        ranked = "Ferro"
+    }
+    else if(balance >= 11 && balance <= 20){
+        ranked = "Bronze"
+    }
+    else if(balance >= 21 && balance <= 50){
+        ranked = "Prata"
+    }
+    else if(balance >= 51 && balance <= 80){
+        ranked = "Ouro"
+    }
+    else if(balance >= 81 && balance <= 90){
+        ranked = "Diamante"
+    }
+    else if(balance >= 91 && balance <= 100){
+        ranked = "Lendário"
+    }
+    else if(balance >= 101){
+        ranked = "Imortal"
+    }
+    else{ ranked = "Indeterminado"}
+
+    let result = [balance, ranked]
+    return result
 }
