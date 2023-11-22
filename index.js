37753a4f3ab406880d6dda95592f557692d12455
ArturRochel <arturrochel@gmail.com>
2023-11-22 08:54:38 -0300

    Segunda ataulização

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Inicio do Projeto")
+let saldo = level(10, 5)
+
+console.log(`O saldo de vítorias desse jogador é ${saldo}`)
+
+function level(victories, defeats){
+    let balance = victories - defeats
+    return balance
+}
